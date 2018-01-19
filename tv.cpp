7c1046827b7790e57eaec4e0be2bce3c725f5a68
JuliArlianto <32285721+JuliArlianto@users.noreply.github.com>
2018-01-19 16:30:33 +0800

    next

--- tv.cpp
+++ tv.cpp
@@ -10,3 +10,36 @@ class tv {
     	void hargatv (int harga);
                 
         };
+    int main(){
+    tv lcd,led,tvtabung,tvplasma; 
+
+	lcd.merktv			="merk toshiba ";
+	led.merktv			="merk samsung ";
+	tvtabung.merktv		="merk lg ";
+	tvplasma.merktv		="merk sharp ";
+	
+	cout << "Tv lcd: \n";
+	cout << (lcd.merktv);
+	lcd.ukurantv("40inch");
+	lcd.hargatv(100000);
+	cout <<"===================================\n";
+	
+	cout << "Tv led: \n";
+	cout << (led.merktv);
+	led.ukurantv("50inch");
+	led.hargatv(200000);
+	cout <<"===================================\n";
+	
+	cout << "Tv tabung: \n";
+	cout << (tvtabung.merktv);
+	tvtabung.ukurantv("60inch");
+	tvtabung.hargatv(300000);
+	cout <<"===================================\n";
+	
+	cout << "Tv plasma: \n";
+	cout << (tvplasma.merktv);
+	tvplasma.ukurantv("70inch");
+	tvplasma.hargatv(400000);
+	cout <<"===================================\n";
+	
+	}
