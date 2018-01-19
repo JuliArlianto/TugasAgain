600152fad03595d516f67fae3b3326ed19e3ca43
JuliArlianto <32285721+JuliArlianto@users.noreply.github.com>
2018-01-19 16:28:03 +0800

    class

---
+++ tv.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class tv {
+    public:
+        void jenistv(string tv);
+        string merktv;
+        void ukurantv(string ukuran);
+    	void hargatv (int harga);
+                
+        };
